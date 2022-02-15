2724b9af104dbd6c8b8b61bb1ad62dd71fe650aa
aydinevren <aydinevren002@gmail.com>
2022-02-15 12:28:00 +0300

    complated project

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
+
+func throwPebble(r float64) func() bool {
+	return func() bool {
+		x := rand.Float64()*2 - 1
+		y := rand.Float64()*2 - 1
+		if x*x+y*y <= r {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+func piFind(n int) float64 {
+	var sumCircle = 0
+	var r = 1.0
+	s := throwPebble(r)
+	for i := 0; i < n; i++ {
+		if s() {
+			sumCircle++
+		}
+	}
+	return 4.0 * float64(sumCircle) / float64(n)
+}
 
 func main() {
-	fmt.Println("Merhaba Github")
+	fmt.Println(piFind(999))
 }
