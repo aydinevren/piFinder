d49358c28187c48c83a34d40d56ca7a1ffb37a3e
aydinevren <aydinevren002@gmail.com>
2022-02-18 17:17:01 +0300

    code refactore

--- main.go
+++ main.go
@@ -3,35 +3,26 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
-func throwPebble(r float64) func() bool {
-	return func() bool {
-		x := rand.Float64()*2 - 1
-		y := rand.Float64()*2 - 1
-		if x*x+y*y <= r {
-			return true
-		} else {
-			return false
-		}
-	}
-}
-
-func piFind(n int, threads int) float64 {
-	threadsNumber := n / threads
+func piFind(n int, threads int) (float64, time.Duration) {
+	batchNumber := n / threads
 	result := make(chan float64, threads)
+	start := time.Now()
+
 	for i := 0; i < threads; i++ {
 		go func() {
-			var sumCircle = 0
-			var r = 1.0
-			s := throwPebble(r)
-			for j := 0; j < threadsNumber; j++ {
-				if s() {
+			var sumCircle int
+			r := rand.New(rand.NewSource(int64(rand.Float64())))
+
+			for j := 0; j < batchNumber; j++ {
+				x, y := r.Float64(), r.Float64()
+				if x*x+y*y <= 1.0 {
 					sumCircle++
 				}
 			}
-			result <- 4.0 * float64(sumCircle) / float64(threadsNumber)
-
+			result <- 4.0 * float64(sumCircle) / float64(batchNumber)
 		}()
 	}
 	var total float64
@@ -39,9 +30,52 @@ func piFind(n int, threads int) float64 {
 		total += <-result
 	}
 
-	return total / float64(threads)
+	passingTime := time.Since(start)
+
+	return total / float64(threads), passingTime
 }
 
 func main() {
-	fmt.Println(piFind(1000000000, 2))
+	resultOfPi, passingTime := piFind(10000000000, 12)
+	fmt.Println("Pi:", resultOfPi, "\nTime: ", passingTime)
 }
+
+/* func throwPebble() func() bool {
+	return func() bool {
+		r := rand.New(rand.NewSource(int64(rand.Float64())))
+		x, y := r.Float64(), r.Float64()
+
+		if x*x+y*y <= 1.0 {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+func piFind(n int, threads int) (float64, time.Duration) {
+	batchNumber := n / threads
+	result := make(chan float64, threads)
+	start := time.Now()
+
+	for i := 0; i < threads; i++ {
+		go func() {
+			var sumCircle int
+			for j := 0; j < batchNumber; j++ {
+				s := throwPebble()
+				if s() {
+					sumCircle++
+				}
+			}
+			result <- 4.0 * float64(sumCircle) / float64(batchNumber)
+		}()
+	}
+	var total float64
+	for i := 0; i < threads; i++ {
+		total += <-result
+	}
+
+	passingTime := time.Since(start)
+
+	return total / float64(threads), passingTime
+} */
